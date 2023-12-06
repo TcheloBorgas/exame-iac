cac1597aa2109243bd8a8fd7be87063260edaf34
Marcelo Henrique Borgas <106122728+TcheloBorgas@users.noreply.github.com>
2023-12-06 20:48:06 -0300

    teste dnv

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -12,6 +12,36 @@ resource "aws_s3_bucket_versioning" "bucket-versioning" {
 }
 
 
+
+resource "aws_s3_bucket_policy" "allow_access" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.allow_access.json
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.bucket.id
+}
+
+
+data "aws_iam_policy_document" "allow_access" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+  }
+}
+
 resource "aws_s3_bucket_website_configuration" "bucket-website-configuration" {
   bucket = aws_s3_bucket.bucket.id
 
