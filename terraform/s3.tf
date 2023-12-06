bf2a41cc01415d68ec1f2a08752a0cfe8f6000f8
Marcelo Henrique Borgas <106122728+TcheloBorgas@users.noreply.github.com>
2023-12-06 20:36:52 -0300

    erra

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -11,35 +11,9 @@ resource "aws_s3_bucket_versioning" "bucket-versioning" {
   }
 }
 
-
-
-resource "aws_s3_bucket_policy" "allow_access" {
+resource "aws_s3_bucket_acl" "bucket-acl" {
   bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.allow_access.json
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.bucket.id
-}
-
-
-data "aws_iam_policy_document" "allow_access" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      aws_s3_bucket.bucket.arn,
-      "${aws_s3_bucket.bucket.arn}/*",
-    ]
-  }
+  # acl    = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "bucket-website-configuration" {
@@ -70,5 +44,3 @@ resource "aws_s3_object" "bucket-objects" {
   content_type = "text/html"
   etag         = md5(file("../app/${each.value}"))
 }
-
-
