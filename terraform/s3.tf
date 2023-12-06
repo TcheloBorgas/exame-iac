2eba0eccae291089b79551e27e6943b31ed39253
Marcelo Henrique Borgas <106122728+TcheloBorgas@users.noreply.github.com>
2023-12-06 20:25:30 -0300

    vamo

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket_versioning" "bucket-versioning" {
 
 resource "aws_s3_bucket_acl" "bucket-acl" {
   bucket = aws_s3_bucket.bucket.id
-  acl    = "public-read"
+  # acl    = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "bucket-website-configuration" {
