75cd2f085c38bb7936fdc84276d4b4d4b2a31d2e
Marcelo Henrique Borgas <106122728+TcheloBorgas@users.noreply.github.com>
2023-12-06 20:15:19 -0300

    subiu

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,7 +11,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "boquetaexame"
+    bucket         = "boquetaexame-nw"
     key            = "terraform.tfstate"
     dynamodb_table = "boquetaexame-db"
     region         = "us-east-1"
